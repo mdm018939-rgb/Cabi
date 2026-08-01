d1a942e6c2fa6f110e9b73d1060eab97fae4b957
mdm018939-rgb <mdm018939@gmail.com>
2026-08-01 19:10:48 +0600

    Improve OTP message formatting
    Updated OTP message formatting to include additional line breaks for better readability.

--- bot.py
+++ bot.py
@@ -223,8 +223,8 @@ while True:
                             f"🪽 <b>SMS Received</b> 🪽\n\n"
                             f"🌐 <b>Service:</b> {service_name}\n"
                             f"🏳️ <b>Country:</b> {country_display}\n"
-                            f"📞 <b>Number:</b> <code>{masked_num}</code>\n"
-                            f"🔑 <b>Code:</b> <code>{otp_code}</code>\n"
+                            f"📞 <b>Number:</b> <code>{masked_num}</code>\n\n"
+                            f"🔑 <b>Code:</b> <code>{otp_code}</code>\n\n"
                             f"<blockquote>{safe_message_content}</blockquote>"
                         )
                         
